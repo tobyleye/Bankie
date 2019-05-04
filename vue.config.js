5a91a8df9bbb96c4ff906dd2d255ee1bdd0ec2a7
tobyleye <oyetobb@gmail.com>
2019-05-04 13:10:32 +0100

    integrate serviceworker plugin

---
+++ vue.config.js
@@ -0,0 +1,14 @@
+// vue.config.js 
+
+const path = require('path');
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin');
+
+module.exports = {
+  configureWebpack: {
+    plugins: [
+      new ServiceWorkerWebpackPlugin({
+        entry: path.join(__dirname, 'src/sw.js'),
+      })
+    ]
+  }
+}
